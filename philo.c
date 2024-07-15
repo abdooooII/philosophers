1e1829b17737364772ac728a19638555bdfda348
Abderrahmane Ouafsou <abouafso@e3r9p2.1337.ma>
2024-07-15 08:04:08 +0100

    .

--- philo.c
+++ philo.c
@@ -6,19 +6,42 @@
 /*   By: abouafso <abouafso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 19:20:45 by abouafso          #+#    #+#             */
-/*   Updated: 2024/07/14 19:48:14 by abouafso         ###   ########.fr       */
+/*   Updated: 2024/07/15 08:01:56 by abouafso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	is_digit(char **str)
+{
+	int i = 1;
+	int	j = 0;
+	
+	while(str[i])
+	{
+		while(str[i][j])
+		{
+			if(!(str[i][j] >= '0' && str[i][j] <= '9'))
+			{
+				printf("arguments contain alphabets");
+				return(1);
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return(0);
+}
+
 int main(int ac, char **av)
 {
-    (void) av;
+    
     if (ac < 5 || ac > 6)
     {
         printf("ERROR : INVALID NUMBER OF ARGUMENTS!\n");
         return(1);
     }
+	is_digit(av);
     
 }
